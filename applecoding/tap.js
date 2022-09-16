df03ee144d305deb41c6780e9fae0e8d6b617547
cheoljin <chur7072@naver.com>
2022-09-16 13:57:17 +0900

    tap for statement

--- applecoding/tap.js
+++ applecoding/tap.js
@@ -4,23 +4,34 @@
 // 모든 div에 붙은 show 클래스명 제거
 // div0에 show 클래스명 추가
 
-$('.tab-button').eq(0).on('click', function(){
-$('.tab-button').removeClass('orange');
-$('.tab-button').eq(0).addClass('orange');
-$('.tab-content').removeClass('show');
-$('.tab-content').eq(0).addClass('show');
-});
-
-$('.tab-button').eq(1).on('click', function(){
-  $('.tab-button').removeClass('orange');
-  $('.tab-button').eq(1).addClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-content').eq(1).addClass('show');
-  });
-
-  $('.tab-button').eq(2).on('click', function(){
+// var 버튼 = $('.tab-button'); 더 빠름
+// 확장성을 잡고싶다면 i < $('.tab-button').length
+for(let i = 0; i < 3; i++) { // let을 싸야 한다.
+  $('.tab-button').eq(i).on('click', function(){
     $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(2).addClass('orange');
+    $('.tab-button').eq(i).addClass('orange');
     $('.tab-content').removeClass('show');
-    $('.tab-content').eq(2).addClass('show');
-    });
+    $('.tab-content').eq(i).addClass('show');
+  });
+}
+
+// $('.tab-button').eq(0).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(0).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(0).addClass('show');
+// });
+
+// $('.tab-button').eq(1).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(1).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(1).addClass('show');
+//   });
+
+// $('.tab-button').eq(2).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(2).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(2).addClass('show');
+//   });
