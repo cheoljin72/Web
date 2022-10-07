c4c4772bdf6a4767f64c5038837907ef32b76cc7
cheoljin <chur7072@naver.com>
2022-10-07 17:24:24 +0900

    update_folderName

---
+++ web/index.js
@@ -0,0 +1,9 @@
+let name1 = 'kimcheoljin';
+let age = 27;
+let hometown = 'pohang';
+let sing = { name: 'buzz', singName: 'monologue' };
+let project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true
+};
---
+++ web/index.ts
@@ -0,0 +1,16 @@
+let name1 : string = 'kimcheoljin';
+let age : number = 27;
+let hometown : string = 'pohang';
+
+let sing : {name : string, singName : string}
+= {name : 'buzz', singName : 'monologue'}
+
+let project : {
+  member : string[],
+  days : number, 
+  started : boolean
+} = {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true
+}
