bbe4b15affa42b85c53b96227763127426b58b91
cheoljin <chur7072@naver.com>
2022-10-19 17:52:15 +0900

    search icon

---
+++ web/js/array-api.js
@@ -0,0 +1,11 @@
+{
+  const fruits = ['apple', 'banana', 'orange']; 
+  const result = fruits.join(); // 배열을 문자열로 바꾸는 api
+  console.log(result);
+}
+
+{
+  const fruits = 'apple,banana,orange';
+  const result = fruits.split(',');
+  console.log(result);
+}
